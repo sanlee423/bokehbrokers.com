9aee947a688905b819a8205dff990b57c1d9dbaf
Sanghwa Lee <david.lee@wheel.com>
2022-06-28 02:17:12 -0400

    revert iframe changes - for now

--- src/components/featuredCard/featuredCard.tsx
+++ src/components/featuredCard/featuredCard.tsx
@@ -1,6 +1,5 @@
-import {CircularProgress} from '@mui/material';
 import {makeStyles} from '@mui/styles';
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import cpTheme from 'src/theme/cpTheme';
 
 const useStyles = makeStyles(theme => ({
@@ -31,29 +30,17 @@ const featuredVideoData = ['eE6xWnLfJho', 'e92dppDp6L8', 'wNNiS6UILS0'];
 export const FeaturedCard: React.FC = () => {
   const classes = useStyles(cpTheme);
 
-  const iFrameContainer = useRef<HTMLDivElement>(null);
   const videoId =
     featuredVideoData[Math.floor(Math.random() * featuredVideoData.length)];
 
-  useEffect(() => {
-    const iFrame = document.createElement('iframe');
-    iFrame.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&color=white&controls=0&showinfo=0&mute=1&disablekb=1&enablejsapi=1&fs=0&modestbranding=1&playlist=${videoId}`;
-    iFrame.className = classes.videoIFrame;
-    iFrame.setAttribute('frameBorder', '0');
-    iFrame.setAttribute('scrolling', 'no');
-    iFrame.setAttribute(
-      'allow',
-      'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-    );
-
-    if (iFrameContainer.current) {
-      iFrameContainer.current.appendChild(iFrame);
-    }
-  }, [classes.videoIFrame, videoId]);
-
   return (
-    <>
-      <div className={classes.videoContainer} ref={iFrameContainer}></div>
-    </>
+    <div className={classes.videoContainer}>
+      <iframe
+        className={classes.videoIFrame}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&enablejsapi=1&loop=1&color=white&controls=0&showinfo=0&disablekb=1&enablejsapi=1&fs=0&modestbranding=1&playlist=${videoId}`}
+        frameBorder="0"
+        scrolling="no"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    </div>
   );
 };
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 import CameraIcon from '../icons/CameraIcon';
+import {useRouter} from 'next/router';
 
 export const Logo: React.FC = () => {
+  const router = useRouter();
+
+  const handleClick = e => {
+    e.preventDefault();
+    router.push('/');
+  };
+
   return (
     <div className="flex w-1/2 mx-2">
-      <Link href="/" passHref>
-        <a>
-          <CameraIcon
-            data-test="icon"
-            className="text-white"
-            width="58"
-            height="58"
-          />
-        </a>
-      </Link>
+      <a onClick={handleClick}>
+        <CameraIcon
+          data-test="icon"
+          className="text-white"
+          width="58"
+          height="58"
+        />
+      </a>
     </div>
   );
 };
