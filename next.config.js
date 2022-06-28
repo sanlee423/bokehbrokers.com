421308469c71d9b10fd78a28f704187b5a679241
Sanghwa Lee <david.lee@wheel.com>
2022-06-28 02:04:55 -0400

    Add page redirects

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ module.exports = withPlugins([], {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/brands/list',
+        destination: '/brands',
+        permanent: true,
+      },
+      {
+        source: '/cameras/list',
+        destination: '/cameras',
+        permanent: true,
+      },
+      {
+        source: '/film/list',
+        destination: '/film',
+        permanent: true,
+      },
+      {
+        source: '/lens/list',
+        destination: '/lens',
+        permanent: true,
+      },
+    ];
+  },
   env: {
     CAMPEDIA_API_URL: process.env.CAMPEDIA_API_URL,
   },
