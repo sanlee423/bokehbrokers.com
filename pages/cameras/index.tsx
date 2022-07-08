9e635a39bc27e1361092fbe5c20f0d5e162e9991
Sanghwa Lee <david.lee@wheel.com>
2022-07-08 19:59:19 -0400

    add pointer

--- pages/cameras/index.tsx
+++ pages/cameras/index.tsx
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
   },
   cameraItem: {
     display: 'flex',
+    cursor: 'pointer',
     flexDirection: 'row',
     justifyContent: 'flex-start',
     padding: '3%',
