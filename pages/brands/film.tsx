2523e89b5e43d6bb1ece52d47c4ef0cdedfd31c3
Sanghwa Lee <david.lee@wheel.com>
2022-07-13 19:17:53 -0400

    make non.empty

---
+++ pages/brands/film.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageList from '@/components/pageComponents/pageList';
+
+const Brands: React.FC = () => {
+  return <PageList type={'brands'} />;
+};
+
+export default Brands;
