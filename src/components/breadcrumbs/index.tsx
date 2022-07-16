7c0df2b372d76a70521120212ddda5785c8efb43
Sanghwa Lee <david.lee@wheel.com>
2022-07-16 02:42:00 -0400

    breadcrumb by width

--- src/components/breadcrumbs/index.tsx
+++ src/components/breadcrumbs/index.tsx
@@ -4,6 +4,7 @@ import Link from '@mui/material/Link';
 import {useRouter} from 'next/router';
 import {makeStyles} from '@mui/styles';
 import {campediaTheme} from '@/utils/campediaTheme';
+import useWindowSize from '@/utils/windowDimensions';
 
 const useStyles = makeStyles(theme => ({
   breadCrumb: {},
@@ -11,6 +12,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Breadcrumb() {
   const classes = useStyles(campediaTheme);
+  const {width} = useWindowSize();
   const {asPath} = useRouter();
   const splitArray = asPath.split('/');
   splitArray[0] = 'home';
@@ -27,39 +29,41 @@ export default function Breadcrumb() {
 
   return (
     <div className={classes.breadCrumb}>
-      <Breadcrumbs aria-label="breadcrumb">
-        {splitArray &&
-          splitArray.map((link, i) => {
-            if (i === 0) {
-              return (
-                <Link key={link} underline="hover" color="inherit" href="/">
-                  Home
-                </Link>
-              );
-            } else if (i === splitArray.length - 1) {
-              return (
-                <Link
-                  key={link}
-                  underline="hover"
-                  color="text.primary"
-                  href={generateLink(asPath, link)}
-                  aria-current="page">
-                  {capitalizeText(link)}
-                </Link>
-              );
-            } else {
-              return (
-                <Link
-                  key={link}
-                  underline="hover"
-                  color="inherit"
-                  href={generateLink(asPath, link)}>
-                  {capitalizeText(link)}
-                </Link>
-              );
-            }
-          })}
-      </Breadcrumbs>
+      {width > 700 && (
+        <Breadcrumbs aria-label="breadcrumb">
+          {splitArray &&
+            splitArray.map((link, i) => {
+              if (i === 0) {
+                return (
+                  <Link key={link} underline="hover" color="inherit" href="/">
+                    Home
+                  </Link>
+                );
+              } else if (i === splitArray.length - 1) {
+                return (
+                  <Link
+                    key={link}
+                    underline="hover"
+                    color="text.primary"
+                    href={generateLink(asPath, link)}
+                    aria-current="page">
+                    {capitalizeText(link)}
+                  </Link>
+                );
+              } else {
+                return (
+                  <Link
+                    key={link}
+                    underline="hover"
+                    color="inherit"
+                    href={generateLink(asPath, link)}>
+                    {capitalizeText(link)}
+                  </Link>
+                );
+              }
+            })}
+        </Breadcrumbs>
+      )}
     </div>
   );
 }
